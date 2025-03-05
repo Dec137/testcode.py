0426b3b76c6351b80a1e6e9d797a749fa6320dc7
Dec137 <166728022+Dec137@users.noreply.github.com>
2025-03-05 02:02:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-import os
 import requests
+from bs4 import BeautifulSoup
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 import logging
@@ -12,23 +12,9 @@ logging.basicConfig(
 )
 
 # Constants
-TELEGRAM_TOKEN = os.getenv('7031299961:AAH_hIZhOMe0wKLK4wZD_W49eVyNJN9tUqM')  # Get token from environment variable
-BROWSERLESS_API_KEY = os.getenv('Rslb2B5jOC8sdzb61784bf0fbc8f4840652f1e7027')  # Browserless API key
+TELEGRAM_TOKEN = '7031299961:AAH_hIZhOMe0wKLK4wZD_W49eVyNJN9tUqM'  # Your Telegram bot token
 TARGET_URL = 'https://giaxstore.com/collections/sconti-selezionati'
 
-# Browserless API endpoint
-BROWSERLESS_URL = f"https://chrome.browserless.io/content?token={BROWSERLESS_API_KEY}"
-
-def main():
-    app = Application.builder().token(TELEGRAM_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("discounts", get_discounts))
-    
-    print("Bot is running...")
-    app.run_polling(
-        drop_pending_updates=True,  # Ignore pending updates
-        poll_interval=10.0  # Poll every 2 seconds
-    )
 # Telegram Bot Commands
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -38,23 +24,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def get_discounts(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        # Fetch and parse HTML using Browserless
-        headers = {
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json',
-        }
-        payload = {
-            "url": TARGET_URL,
-            "waitFor": 5000,  # Wait for 5 seconds to ensure the page loads
-            "gotoOptions": {
-                "waitUntil": "networkidle2"  # Wait until the network is idle
-            }
-        }
-        response = requests.post(BROWSERLESS_URL, headers=headers, json=payload)
-        response.raise_for_status()
-
-        # Parse the HTML content
-        from bs4 import BeautifulSoup
+        # Fetch and parse HTML
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(TARGET_URL, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # Extract product data
